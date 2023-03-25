4aa2800ec5f9e0ff82d831a807c7e63321af4d5e
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-03-25 07:52:49 -0700

    Math and comparison operators assignment

---
+++ MathAndComparisonOperatorsAssignment/MathAndComparisonOperatorsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MathAndComparisonOperatorsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program\nPerson 1\nHourly Rate:");
+            string HourlyRate1 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week:");
+            string WeeklyHours1 = Console.ReadLine();
+            Console.WriteLine("Person 2:\nHourly Rate:");
+            string HourlyRate2 = Console.ReadLine();
+            Convert.ToDouble(HourlyRate2);
+            Console.WriteLine("Hours worked per week:");
+            string WeeklyHours2 = Console.ReadLine();
+            Convert.ToDouble(WeeklyHours2);
+            decimal AnnSal1 = Convert.ToDecimal(HourlyRate1) * Convert.ToDecimal(WeeklyHours1) * 50.714m;
+            Console.WriteLine("Annual salary of Person 1: " + AnnSal1);
+            decimal AnnSal2 = Convert.ToDecimal(HourlyRate2) * Convert.ToDecimal(WeeklyHours2) * 50.714m;
+            Console.WriteLine("Annual salary of Person 2: " + AnnSal2);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool comparison = AnnSal1 > AnnSal2;
+            Console.WriteLine(comparison);
+        }
+    }
+}
