e61c573d748ef99c7899680fe572f5b04f163aa1
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-03-25 08:13:51 -0700

    Boolean Logic Assignment

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUI = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            bool qualify = Convert.ToByte(age) > 15 && Convert.ToBoolean(DUI) == false && Convert.ToByte(tickets) < 3;
+            Console.WriteLine("Qualifed?\n" + qualify);
+        }
+    }
+}
