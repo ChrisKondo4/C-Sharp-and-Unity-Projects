8d5b73521d4402c7853a366210d8527f5ce6a940
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-03-25 10:39:36 -0700

    Console App assignment

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1, 2, or 3?");
+            byte num1 = Convert.ToByte(Console.ReadLine());
+
+            while (num1 == 1 || num1 == 2)
+            {
+                Console.WriteLine("You chose incorrectly.");
+                num1 = Convert.ToByte(Console.ReadLine());
+                break;
+            }
+
+            Console.WriteLine("1, 2, or 3? For real this time.");
+            num1 = Convert.ToByte(Console.ReadLine());
+            bool objective = false;
+
+            do
+            {
+                switch (num1)
+                {
+                    case 1:
+                        Console.WriteLine("No. Try again.");
+                        num1 = Convert.ToByte(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("No. Try again.");
+                        num1 = Convert.ToByte(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("Yes!");
+                        objective = true;
+                        break;
+                }
+            }
+            while (!objective);
+            Console.ReadLine();
+        }
+    }
+}
