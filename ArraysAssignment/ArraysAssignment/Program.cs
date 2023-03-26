432acc9fa3259dc14a05f02f4bdc55c3cde5e058
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-03-26 10:03:41 -0700

    ArraysAssignment

---
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = { "Hello, ", "my name is ", "whatever", "you decide!" };
+            int[] intArray = { 4, 8, 16, 32 };
+            List<string> stringList = new List<string>();
+            stringList.Add("Hello, ");
+            stringList.Add("my name is ");
+            stringList.Add("whatever ");
+            stringList.Add("you decide!");
+
+            Console.WriteLine("Hello! Please select an index of 0-3.");
+            byte index = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(stringArray[index]);
+
+            Console.WriteLine("Fantastic! Now please select a new index of 0-3.");
+            index = Convert.ToByte(Console.ReadLine());
+
+            if (index >= 0 && index <= 3)
+            {
+                Console.WriteLine(intArray[index]);
+            }
+            else
+            {
+                Console.WriteLine("That index doesn't exist.");
+            }
+            Console.WriteLine("Okay, now one last time: please select an index of 0-3.");
+            index = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(stringList[index]);
+        }
+    }
+}
