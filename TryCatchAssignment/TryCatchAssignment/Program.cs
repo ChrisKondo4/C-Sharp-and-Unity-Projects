d1e3356842756e60fff4d8d8db30e9e9a47fea08
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-03-27 22:25:08 -0700

    Try/Catch assignment

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> integers = new List<int>();
+                integers.Add(400);
+                integers.Add(150);
+                integers.Add(200);
+                integers.Add(500);
+
+                Console.WriteLine("Please choose a whole number");
+                int dividedBy = Convert.ToInt32(Console.ReadLine());
+                foreach (int index in integers)
+                {
+                    Console.WriteLine(index / dividedBy);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Don't enter 0");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Enter a number, silly!");
+            }
+            finally
+            {
+                Console.WriteLine("Let's try that again, shall we?");
+            }
+        }
+    }
+}
