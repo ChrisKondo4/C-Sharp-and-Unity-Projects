9db929261333cf89ecec98a77774d2622ca72c35
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-07 02:44:45 -0700

    Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Alright, now which number shall we start with?");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's the second number?");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Which math operation would you like to perform: addition, subtraction, or multiplication?");
+            string operation = Console.ReadLine();
+            operation.ToLower();
+            if (operation == "addition")
+            {
+                Console.WriteLine(Addition(userInput1, userInput2));
+            }
+            if (operation == "subtraction")
+            {
+                Console.WriteLine(Subtraction(userInput1, userInput2));
+            }
+            if (operation == "multiplication")
+            {
+                Console.WriteLine(Multiplication(userInput1, userInput2));
+            }
+        }
+
+        static int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CallingMethodsAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CallingMethodsAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21a65df8-5564-4d29-b6c8-3232b4da35ef")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
