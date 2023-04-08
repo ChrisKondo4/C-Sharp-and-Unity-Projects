0666cb207c42457a6ec6605b6d61002016f3cd6a
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-07 19:23:44 -0700

    method class

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methodology(9, 6);
+            Methodology(y: 6, x: 9);
+        }
+        
+        static void Methodology(int x, int y)
+        {
+            int l = x / 2;
+            Console.WriteLine(y);
+        }
+    }
+}
