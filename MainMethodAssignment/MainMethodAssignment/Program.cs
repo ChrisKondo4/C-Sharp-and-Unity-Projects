cdba85fe625b45cbdeb498bc2955516adbab5676
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-07 19:06:24 -0700

    Main Method

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Additin:");
+            Console.WriteLine(addition(5));
+            Console.WriteLine(addition(5.4m));
+            Console.WriteLine(addition("five"));
+        }
+
+        static int addition(int x)
+        {
+            return x + 10;
+        }
+
+        static int addition(decimal x)
+        {
+            return Convert.ToInt32(x * 7.5m);
+        }
+
+        static int addition(string x)
+        {
+            return Convert.ToInt32(x);
+        }
+    }
+}
