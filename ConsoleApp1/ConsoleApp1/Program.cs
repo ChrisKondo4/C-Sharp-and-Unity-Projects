1313545a5e78c6aca7a287b82c2f8e61951bacc6
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-07 20:02:32 -0700

    Class assignment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please give me a number!");
+            int x = Convert.ToInt32(Console.ReadLine());
+            New(x);
+        }
+        
+        static void New(int x)
+        {
+            Console.WriteLine(x / 2);
+        }
+
+        static void New(string x)
+        {
+            Console.WriteLine(x + " is not an okay thing to say.");
+        }
+    }
+}
