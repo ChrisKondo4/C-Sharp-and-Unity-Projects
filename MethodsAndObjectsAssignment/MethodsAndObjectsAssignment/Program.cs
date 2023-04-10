e05a5d35647caf1d2d1c8e722ca8802af09d7954
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-10 06:04:06 -0700

    methods and objects

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ChrisK = new Employee();
+            ChrisK.FirstName = "Joshu";
+            ChrisK.LastName = "Kondo";
+            ChrisK.SayName();
+        }
+    }
+}
