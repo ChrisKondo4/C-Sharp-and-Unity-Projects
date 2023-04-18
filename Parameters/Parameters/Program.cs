2a565152e6f69d756cd1401833ca35a7bf1334fe
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-18 14:25:55 -0700

    Parameters

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Chris = new Employee<string>()
+            {
+                Things = new List<string> { "computer", "clothes", "phone" }
+            };
+            Employee<int> ChrisQuantity = new Employee<int>()
+            {
+                Things = new List<int> { 1, 5, 3, 9, 4, 8 }
+            };
+            foreach (string list in Chris.Things)
+            {
+                Console.WriteLine(list);
+            }
+            foreach (int list in ChrisQuantity.Things)
+            {
+                Console.WriteLine(list);
+            }
+            Console.ReadLine();
+        }
+    }
+}
