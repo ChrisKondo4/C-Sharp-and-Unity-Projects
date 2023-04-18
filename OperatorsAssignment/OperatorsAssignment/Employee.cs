e51cbc81cb01eefa3ff25c6738ad57cd06432fa6
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-18 12:53:29 -0700

    operators assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator == (Employee Uno, Employee Dos)
+        {
+            return Uno == Dos;
+        }
+
+        public static bool operator != (Employee Uno, Employee Dos)
+        {
+            return Uno != Dos;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Uno = new Employee
+            {
+                Id = 0,
+                Name = "Jerry"
+            };
+            Employee Dos = new Employee
+            {
+                Id = 1,
+                Name = "Harry"
+            };
+            Uno == Dos;
+        }
+    }
+}
