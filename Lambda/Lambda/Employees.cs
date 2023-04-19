78010ec4fc94a6304e9211b2e728b30d8a3e0ffd
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-19 05:46:27 -0700

    Lambda

---
+++ Lambda/Lambda/Employees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Employees
+    {
+        private int Id;
+        private string firstName;
+        private string lastName;
+
+        public Employees(int v1, string v2, string v3)
+        {
+            this.Id = v1;
+            this.firstName = v2;
+            this.lastName = v3;
+        }
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employees> currentEmployees = new List<Employees>();
+            currentEmployees.Add(new Employees(0, "Joe", "Smith"));
+            currentEmployees.Add(new Employees(1, "Harry", "Styles"));
+            currentEmployees.Add(new Employees(2, "Richard", "Patten"));
+            currentEmployees.Add(new Employees(3, "Mary", "Sother"));
+            currentEmployees.Add(new Employees(4, "Bob", "Leamen"));
+            currentEmployees.Add(new Employees(5, "Joe", "Marraro"));
+            currentEmployees.Add(new Employees(6, "Pi'erre", "Bourne"));
+            currentEmployees.Add(new Employees(7, "Sierra", "Mist"));
+            currentEmployees.Add(new Employees(8, "Travis", "Scott"));
+            currentEmployees.Add(new Employees(9, "Sarah", "Baker"));
+
+            List<Employees> theJoes = new List<Employees>();
+
+            foreach (Employees employee in currentEmployees)
+            {
+                if (employee.firstName = "Joe")
+                {
+                    theJoes.Add(employee);
+                }
+            }
+        }
+    }
+}
