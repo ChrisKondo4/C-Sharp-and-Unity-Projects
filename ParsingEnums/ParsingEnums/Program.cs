2930fb06aa642f64d3dc0ee7d18dca0a8e53757a
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-19 03:38:11 -0700

    Parsing Enums

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter which day of the week it is!");
+            try
+            {
+                string Input = Console.ReadLine();
+                DaysOfTheWeek Today = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek.Day), Input);
+                Console.WriteLine("Today is: " + Today);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("That is not a day of the week.");
+            }
+            finally
+            {
+                Console.WriteLine("Please enter which day of the week it is!");
+            }
+        }
+    }
+}
