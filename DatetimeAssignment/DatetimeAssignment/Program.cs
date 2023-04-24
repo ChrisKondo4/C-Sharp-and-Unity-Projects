0ec3a774247286879f92088fff0c5fe2ec1f7470
Killiest <99295116+ChrisKondo4@users.noreply.github.com>
2023-04-23 21:26:18 -0700

    Datetime

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current time: " + DateTime.Now);
+            Console.WriteLine("Please enter a number!");
+            double time = Convert.ToDouble(Console.ReadLine());
+            DateTime converted = DateTime.Now;
+            Console.WriteLine(converted.AddHours(time));
+        }
+    }
+}
